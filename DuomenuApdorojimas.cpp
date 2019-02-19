f6cee8cfa9f982ee889f535ed34f686fe2dd9d54
Effanuel <etajuozas@gmail.com>
2019-02-19 20:30:25 +0200

    Version v0.3

--- DuomenuApdorojimas.cpp
+++ DuomenuApdorojimas.cpp
@@ -19,69 +19,32 @@
 
 int main()
 {
-
+	cout << "Ivedus mokinio varda, pavarde, 'n' namu darbu balus\nir egzamino bala, bus isvedama lentele.\n" << std::string(35, '=') << endl;
 	
-	cout << "Ivedus mokinio varda, pavarde, 'n' namu darbu balus\nir egzamino bala, bus isvedama lentele.\n";
-	cout << std::string(35, '=') << endl;
+	int max_vardas{}, max_pavarde{};  //reikia, kad formatavimas butu lengvesnis
+	vector<string> vardai{}, pavardes{}; //
 
-	char read;
-	cout << "Skaityti data is failo?(y/n)" << endl;
-	cin >> read;
-
-	int max_vardas;  //reikia, kad formatavimas butu lengvesnis
-	int max_pavarde;
-	vector<Items> Studentai;
-	if (read == 'y' || read == 'Y') {
-		vector<string> vardai;
-		vector<string> pavardes;
-		Studentai = fail_work("kursiokai.txt", vardai, pavardes);
-
-		max_vardas = max_len(vardai); //reikia, kad formatavimas butu lengvesnis
-		max_pavarde = max_len(pavardes);
-	}
-	else {
-		string vardas;
-		string pavarde;
-		cout << "Iveskite mokinio varda ir pavarde: " << endl;
-		cin >> vardas >> pavarde;
+	vector<Items> Studentai{};
+	vector<int> balai{};
+	balai.reserve(2);
 
 
-		char is_random;
-		cout << "Ar norite, kad balai butu generuojami atsitiktinai?(y/n)" << endl;
-		cin >> is_random;
-
-
-		int egz;
-		vector<int> balai;
-		balai.reserve(2);
-		ar_atsitiktinis(is_random, balai, egz);
-
-		size_t dydis_balu = balai.size() != 0 ? balai.size() : 1;
-		const double galutinis_vid = 0.4 * std::accumulate(balai.begin(), balai.end(), 0.0) / dydis_balu + 0.6 * egz;
-		const double galutinis_med = 0.4 * median(balai, dydis_balu) + 0.6 * egz;
-
-		
-		Studentai.push_back({ vardas, pavarde, galutinis_med, galutinis_vid, egz });  //vieno studento data
-
-		max_vardas = vardas.length(); //reikia, kad formatavimas butu lengvesnis
-		max_pavarde = pavarde.length();
+	char choice{};
+	cout << "1 - Skaityti data is failo;\n2 - Ivesti data rankomis;"<< endl;
+	cin >> choice;
+	switch (choice) {
+	case '1':
+		Failo_nuskaitymas(Studentai, "kursiokai.txt", vardai, pavardes);
+		break;
+	case '2':
+		Duomenu_ivedimas(Studentai, balai, vardai, pavardes);
+		break;
 	}
 
+	max_vardas = max_len(vardai); //reikia, kad formatavimas butu lengvesnis
+	max_pavarde = max_len(pavardes);
 
-
-
-
-
-
-
-	printf("%s\n", std::string((max_pavarde + max_vardas) + 14 + 14 + 12 + 8, '-').c_str());
-	printf("%*s %*s %*s %s\n", -int(max_pavarde) - 7, "Pavarde", -int(max_vardas) - 7, "Vardas", -19, "VidGalutinis", "MedGalutinis");
-	printf("%s\n", std::string((max_pavarde + max_vardas) + 14 + 14 + 12 + 8, '-').c_str());
-
-	for (auto& line : Studentai) {
-		printf("%*s %*s %*.2f %.2f\n", -int(max_pavarde) - 7, line.pavarde.c_str(), -int(max_vardas) - 7, line.vardas.c_str(), -19, line.final_vid, line.final_med);
-	}
-	printf("%s\n", std::string((max_pavarde + max_vardas) + 14 + 14 + 12 + 8, '-').c_str());
+	Print_table(Studentai, max_vardas, max_pavarde);
 	
 	
 
--- mano_func.h
+++ mano_func.h
@@ -18,10 +18,10 @@ using std::string;
 
 struct Items
 {
-	string vardas, pavarde;
-	double final_med;
-	double final_vid;
-	int egz;
+	string vardas{}, pavarde{};
+	double final_med{}, final_vid{};
+	vector<int> balai{};
+	int egz{};
 
 
 };
@@ -44,53 +44,62 @@ bool alphabetical_sorting(const Items &a, const Items &b)
 int cin_and_checkFormat() {
 	int input;
 	while (true) {
-		//!(cin >> input) && input != 0 && !int(input)
-		cin >> input;
-		if (cin.fail())
-		{
+		try {
+			cin >> input;
+			if (cin.fail()) throw 1;
+			return input;
+		} catch (int err) {
 			cout << "Netinkamas balo formatas. Iveskite is naujo:\n";
 			cin.clear();
 			cin.ignore(1000, '\n');
 		}
-		else {
-			return input;
-		}
+		
+
 
 	}
 }
 
 
-void ar_atsitiktinis(char a, vector<int>& balai,  int& egz) {
-	if (a == 'y' || a == 'Y') {
-		srand(time(NULL));
-		balai.resize(5); //5 namu darbai
-		generate(balai.begin(), balai.end(), []() {
-			return rand() % 10 + 1;
-		});
-		egz = rand() % 10 + 1;
-	}
-	else {
+void Rankinis_ivedimas(vector<int>& balai,  int& egz) {
 
-		cout << "Iveskite namu darbu rezultatus(0 arba raide sustabdys ivedinejima): " << endl;
-		int ndbalai;
-		while (cin >> ndbalai && ndbalai != 0) { //int(ndbalai)
-			balai.push_back(ndbalai);
-		}
-		if (balai.empty()) { //error handling
-			balai.resize(1);
-		}
-		cin.clear();
-		cin.ignore(1000, '\n');
-		cout << "Egzamino rezultatas: " << endl;
-		egz = cin_and_checkFormat();
+	cout << "Iveskite namu darbu rezultatus(0 arba raide sustabdys ivedinejima): " << endl;
+	int ndbalai{};
+	while (cin >> ndbalai && ndbalai != 0) { //int(ndbalai)
+		balai.push_back(ndbalai);
 	}
+	if (balai.empty()) { //error handling
+		balai.resize(1);
+	}
+	cin.clear();
+	cin.ignore(1000, '\n');
+	cout << "Egzamino rezultatas: " << endl;
+	egz = cin_and_checkFormat();
+	
 }
 
 
 
-double median(std::vector<int> arr, int size) {
+double median(std::vector<int> arr, size_t size, int egz) {
 	sort(arr.begin(), arr.end());
-	return size % 2 != 0 ? arr[size / 2] : (arr[size / 2 - 1] + arr[size / 2]) / 2;
+	if (size == 0) return 0.6 * egz;
+	const double med = size % 2 != 0 ? arr[size / 2] : (arr[size / 2 - 1] + arr[size / 2]) / 2;
+	return 0.4 * med + 0.6 * egz;
+}
+
+double vidurkis(std::vector<int> arr, size_t size, int egz) {
+	if (size == 0) return 0.6 * egz;
+	const double vid = std::accumulate(arr.begin(), arr.end(), 0.0) / size;
+	return 0.4 * vid + 0.6 * egz;
+}
+
+
+
+void Sukurti_studenta(vector<Items>& Studentai, string vardas, string pavarde, vector<int> balai, int egz) { //&
+	size_t dydis_balu = balai.size() ? balai.size() : 1;
+	const double galutinis_vid = vidurkis(balai, dydis_balu, egz);
+	const double galutinis_med = median(balai, dydis_balu, egz);
+
+	Studentai.push_back({ vardas, pavarde, galutinis_med, galutinis_vid, balai, egz });
 }
 
 
@@ -98,56 +107,109 @@ double median(std::vector<int> arr, int size) {
 
 
 
-vector<Items> fail_work(string file_name, vector<string>& vardai, vector<string>& pavardes) {
-	vector<Items> it;
-	string vardas, pavarde;
-	vector<int> balai;
-	int egz;
-	double galutinis_vid;
-	double galutinis_med;
+void Failo_nuskaitymas(vector<Items>& Studentai, string file_name, vector<string>& vardai, vector<string>& pavardes) {
+	string vardas{}, pavarde{};
+	vector<int> balai{};
+	int egz{};
+	double galutinis_vid{};
+	double galutinis_med{};
 
 	std::ifstream failas(file_name.c_str());
 
 	failas.ignore(1000, '\n');
-
-
-	if (failas.fail()) {
-		std::cerr << "Error opening file" << endl;
-		exit(1);
+	try {
+		if (failas.fail()) throw "Error opening file";
 	}
-	int balas;
-	while (failas.good()) { //good
-		failas >> pavarde >> vardas; //fix?
-		it.reserve(2);
+	catch (...) {
+		cout << "klaida" << endl;
+		exit(0);
+
+	}
+
+
+
+	int balas_temp;
+	while (failas.good()) {
+
+		failas >> pavarde >> vardas;
+		Studentai.reserve(2);
 		for (int j = 0; j < 5; j++) {
-			failas >> balas;
-			if (failas.fail() && !failas.eof()) {
-				std::cerr << "Error. Blogas failo formatas" << endl;
-				exit(2);
-			}
-			else {
-				balai.push_back(balas);
-			}
-				
+			failas >> balas_temp;
+			balai.push_back(balas_temp);
 		}
-		failas >> egz;
-		if (failas.fail() && !failas.eof()) {
-			std::cerr << "Error. Blogas failo formatas" << endl;
-			exit(2);
+		try {
+			if (((!(failas >> egz)) && (failas.fail()) || balai.size() != 5) && !failas.eof()) throw 1; // check if push_back was successful && failas >> egz && not end of line;
 		}
-
-		size_t dydis_balu = balai.size() ? balai.size() : 1;
-		galutinis_vid = 0.4 * std::accumulate(balai.begin(), balai.end(), 0.0) / dydis_balu + 0.6 * egz;
-		galutinis_med = 0.4 * median(balai, dydis_balu) + 0.6 * egz;
-
-		it.push_back({ vardas, pavarde, galutinis_med, galutinis_vid, egz });
+		catch (int err) {
+			cout << "Error blogas formatas: " << err << endl;
+			exit(0);
+		}
+		Sukurti_studenta(Studentai, vardas, pavarde, balai, egz);
+		balai.clear();
 		vardai.push_back(vardas);
 		pavardes.push_back(pavarde);
 	}
-	sort(it.begin(), it.end(), alphabetical_sorting);
+	sort(Studentai.begin(), Studentai.end(), alphabetical_sorting);
+	failas.close(); //nezinau ar butina
+}
 
-	failas.close();
-	return it;
+void random_num_generator(vector<int>& balai, int& egz) {
+	srand(time(NULL));
+	balai.resize(5); //5 namu darbai
+	generate(balai.begin(), balai.end(), []() {
+		return rand() % 10 + 1;
+	});
+	egz = rand() % 10 + 1;
 }
 
 
+
+
+void Duomenu_ivedimas(vector<Items>& Studentai, vector<int>& balai, vector<string>& vardai, vector<string>& pavardes) {
+	//balai.reserve(2);
+	int egz{};
+
+	string vardas{};
+	string pavarde{};
+	cout << "Iveskite mokinio varda ir pavarde: " << endl;
+	cin >> vardas >> pavarde;
+	
+
+	char is_random;
+	cout << "Ar norite, kad balai butu generuojami atsitiktinai?(y/n)" << endl;
+	cin >> is_random;
+
+	switch (tolower(is_random)){
+	case 'y':
+		random_num_generator(balai, egz);
+		break;
+	default:
+		Rankinis_ivedimas(balai, egz);
+		break;
+	}
+
+	vardai.push_back(vardas);
+	pavardes.push_back(pavarde);
+	Sukurti_studenta(Studentai, vardas, pavarde, balai, egz);
+
+}
+
+
+
+
+
+
+
+
+
+
+void Print_table(vector<Items> Studentai, int max_vardas, int max_pavarde) {
+	printf("%s\n", std::string((max_pavarde + max_vardas) + 14 + 14 + 12 + 8, '-').c_str());
+	printf("%*s %*s %*s %s\n", -int(max_pavarde) - 7, "Pavarde", -int(max_vardas) - 7, "Vardas", -19, "VidGalutinis", "MedGalutinis");
+	printf("%s\n", std::string((max_pavarde + max_vardas) + 14 + 14 + 12 + 8, '-').c_str());
+
+	for (auto& line : Studentai) {
+		printf("%*s %*s %*.2f %.2f\n", -int(max_pavarde) - 7, line.pavarde.c_str(), -int(max_vardas) - 7, line.vardas.c_str(), -19, line.final_vid, line.final_med);
+	}
+	printf("%s\n", std::string((max_pavarde + max_vardas) + 14 + 14 + 12 + 8, '-').c_str());
+}
